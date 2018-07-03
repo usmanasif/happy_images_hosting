b88af806bea9daadc958355290bfd8c59b4faac6
Usman Asif <usman.asif@devsinc.com>
2018-07-03 17:43:19 +0500

    ImagesController: Implemented update action and its necessities

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_gallery
+  before_action :set_image, only: [:update, :destroy]
 
   def bulk_create
     if @gallery.update(gallery_params)
@@ -11,9 +12,17 @@ class ImagesController < ApplicationController
     redirect_to root_path
   end
 
-  def destroy
-    @image = @gallery.images.find(params[:id])
+  def update
+    if @image.update(image_params)
+      flash[:notice] = 'Image have been updated successfully.'
+    else
+      flash[:alert] = "You don't have permissions to perform this action."
+    end
 
+    redirect_to root_path
+  end
+
+  def destroy
     if @image.present?
       @image.destroy
       flash[:notice] = 'Image have been deleted from your gallery successfully.'
@@ -29,7 +38,15 @@ class ImagesController < ApplicationController
       params.fetch(:gallery).permit(images_attributes: [:file, :_destroy])
     end
 
+    def image_params
+      params.fetch(:image).permit(:file)
+    end
+
     def set_gallery
       @gallery = Gallery.find_by(ip_address: request.remote_ip)
+    end
+
+    def set_image
+      @image = @gallery.images.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :galleries, only: [:show]
-  resources :images, only: [:create, :destroy] do
+  resources :images, only: [:update, :destroy] do
     collection do
       patch :bulk_create
     end
